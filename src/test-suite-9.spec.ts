8031514c1c9130ec24081b37c42a2c27ca409456
Andras Eszes <andras.eszes@bitrise.io>
2025-07-09 08:41:00 +0200

    feat: thousands of failed tests

--- src/test-suite-9.spec.ts
+++ src/test-suite-9.spec.ts
@@ -1,11 +1,23 @@
 import { faker } from '@faker-js/faker';
 import { describe, expect, test } from 'vitest';
 
-const describes = Array.from({ length: faker.number.int({ min: 3, max: 10 }) }).map(() => ({ title: faker.company.catchPhrase() }));
-const tests = Array.from({ length: faker.number.int({ min: 5, max: 15 }) }).map((_, i) => ({ title: faker.hacker.phrase(), index: i + 1 }));
+const describes = Array.from({ length: faker.number.int({ min: 20, max: 40 }) }).map(() => ({ title: faker.company.catchPhrase() }));
+const tests = Array.from({ length: faker.number.int({ min: 20, max: 40 }) }).map((_, i) => ({ title: faker.hacker.phrase(), index: i + 1 }));
 
 describe.each(describes)('$title', () => {
   test.each(tests)(`$title`, ({ index }) => {
-    expect(true).toBe(index % 2 === 0);
+    const actual = faker.helpers.multiple(() => ({
+      message: faker.hacker.phrase(),
+      summary: faker.hacker.phrase(),
+      description: faker.hacker.phrase(),
+    }), { count: { min: 2, max: 5 } });
+
+    const expected = index % 2 === 0 ? actual : faker.helpers.multiple(() => ({
+      message: faker.hacker.phrase(),
+      summary: faker.hacker.phrase(),
+      description: faker.hacker.phrase(),
+    }), { count: { min: 2, max: 5 } });
+
+    expect(actual).toEqual(expected);
   });
 });
