7213e05793a4fd0932d1ec0fada5310052668b08
Andras Eszes <andras.eszes@bitrise.io>
2025-07-24 16:24:24 +0200

    feat: 5000/50000 failed tests

--- src/test-suite-4.spec.ts
+++ src/test-suite-4.spec.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { expect, test } from 'vitest';
 
-const tests = Array.from({ length: 6500 }).map((_, i) => ({ title: faker.hacker.phrase(), index: i + 1 }));
+const tests = Array.from({ length: 5000 }).map((_, i) => ({ title: faker.hacker.phrase(), index: i + 1 }));
 
 test.each(tests)(`$title`, ({ index }) => {
   const actual = faker.helpers.multiple(() => ({
@@ -10,7 +10,7 @@ test.each(tests)(`$title`, ({ index }) => {
     description: faker.hacker.phrase(),
   }), { count: { min: 2, max: 5 } });
 
-  const expected = index % 8 === 0 ? faker.helpers.multiple(() => ({
+  const expected = index % 10 === 0 ? faker.helpers.multiple(() => ({
     message: faker.hacker.phrase(),
     summary: faker.hacker.phrase(),
     description: faker.hacker.phrase(),
